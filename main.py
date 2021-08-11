378f5f9c0682e69ca921c04f67aa6a9ac8c98d66
Tanvi Kachakayala <84879323+tk-geek@users.noreply.github.com>
2021-08-11 23:31:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+answer=input("what is your name ")
+if (answer=="tanvi"):
+  print("hello " + answer)
+  # begin video if
+  video = input ("when is the next best of ?")
+  if  (video == " 2 days"):
+    print("yay!")
+  else:
+      print("boo!")
+      # end video if
+elif (answer== "iron man") :
+  print("hello" + answer)
+else:
+  print("i don't know you ")
